8cd82c880144e0cfbde25a76ba21d315a713a75f
Olorunda Victory <chidiolorunda@gmail.com>
2025-06-21 10:35:02 +0100

    Finished Advice Generator Web app

---
+++ script.js
@@ -0,0 +1,17 @@
+const adviceDescription = document.getElementById("description");
+const adviceDiceBtn = document.getElementById("dice-button");
+const adviceTitle = document.getElementById("advice-title");
+
+adviceDiceBtn.addEventListener("click", async () => {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    adviceDescription.textContent = `"${data.slip.advice}"`;
+    adviceTitle.textContent = `ADVICE #${data.slip.id}`;
+  } catch (error) {
+    adviceDescription.textContent =
+      "Failed to fetch advice. Please try again later.";
+    console.error("Error fetching advice:", error);
+  }
+});
+
